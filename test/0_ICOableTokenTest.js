6176ee249bccb143448cb048884a96fc1d6d437a
libertylocked <unlock.liberty@gmail.com>
2018-01-24 06:37:00 -0500

    Cleaner test

--- test/0_ICOableTokenTest.js
+++ test/0_ICOableTokenTest.js
@@ -5,6 +5,26 @@ const bn = require("bignumber.js")
 const { assertRevert } = require("./helper")
 
 contract("ICOableToken", (accounts) => {
+  let tradableInstance, nonTradableInstance;
+
+  beforeEach("deploy an immediately tradable token", async () => {
+    let tradableAfter = new Date();
+    tradableAfter.setHours(tradableAfter.getHours() - 1);
+    const tradableAfterUnix = Math.round(tradableAfter.getTime() / 1000);
+    tradableInstance = await ICOableToken.new(1000000000,
+      "bullshit token", 3, "BST", tradableAfterUnix, accounts[0], {
+        from: accounts[0]
+      })
+  })
+  beforeEach("deploy a not yet tradable token", async () => {
+    let tradableAfter = new Date();
+    tradableAfter.setDate(tradableAfter.getDate() + 1);
+    const tradableAfterUnix = Math.round(tradableAfter.getTime() / 1000);
+    nonTradableInstance = await ICOableToken.new(1000000000,
+      "bullshit token", 3, "BST", tradableAfterUnix, accounts[0], {
+        from: accounts[0]
+      })
+  })
   describe("constructor", () => {
     it("should set all the parameters correctly", async () => {
       // tradable 1 day after token deployment
@@ -30,13 +50,7 @@ contract("ICOableToken", (accounts) => {
   describe("transfer", () => {
     it("should allow transfer from the seller during trade lock", async () => {
       // tradable 1 day after token deployment
-      let tradableAfter = new Date();
-      tradableAfter.setDate(tradableAfter.getDate() + 1);
-      const tradableAfterUnix = Math.round(tradableAfter.getTime() / 1000);
-      const instance = await ICOableToken.new(1000000000,
-        "bullshit token", 3, "BST", tradableAfterUnix, accounts[0], {
-          from: accounts[0]
-        })
+      const instance = nonTradableInstance
       const tx = await instance.transfer(accounts[1], 10, { from: accounts[0] })
       assert.equal(tx.logs[0].event, "Transfer")
       assert.equal(tx.logs[0].args._from, accounts[0])
@@ -48,13 +62,7 @@ contract("ICOableToken", (accounts) => {
         new bn(1000000000 - 10).toString())
     })
     it("should not allow transfer from non-seller during trade lock", async () => {
-      let tradableAfter = new Date();
-      tradableAfter.setDate(tradableAfter.getDate() + 1);
-      const tradableAfterUnix = Math.round(tradableAfter.getTime() / 1000);
-      const instance = await ICOableToken.new(1000000000,
-        "bullshit token", 3, "BST", tradableAfterUnix, accounts[0], {
-          from: accounts[0]
-        })
+      const instance = nonTradableInstance
       await instance.transfer(accounts[1], 10, { from: accounts[0] })
       try {
         await instance.transfer(accounts[2], 10, { from: accounts[1] })
@@ -65,13 +73,7 @@ contract("ICOableToken", (accounts) => {
     })
     it("should allow transfer from non-seller once trade is open", async () => {
       // make it immediately tradable
-      let tradableAfter = new Date();
-      tradableAfter.setHours(tradableAfter.getHours() - 1);
-      const tradableAfterUnix = Math.round(tradableAfter.getTime() / 1000);
-      const instance = await ICOableToken.new(1000000000,
-        "bullshit token", 3, "BST", tradableAfterUnix, accounts[0], {
-          from: accounts[0]
-        })
+      const instance = tradableInstance
       await instance.transfer(accounts[1], 10, { from: accounts[0] })
       const tx = await instance.transfer(accounts[2], 10, { from: accounts[1] })
       assert.equal(tx.logs[0].event, "Transfer")
@@ -86,13 +88,7 @@ contract("ICOableToken", (accounts) => {
   })
   describe("transfer from", () => {
     it("should allow transfer from seller during trade lock, when allowance is given to seller", async () => {
-      let tradableAfter = new Date();
-      tradableAfter.setDate(tradableAfter.getDate() + 1);
-      const tradableAfterUnix = Math.round(tradableAfter.getTime() / 1000);
-      const instance = await ICOableToken.new(1000000000,
-        "bullshit token", 3, "BST", tradableAfterUnix, accounts[0], {
-          from: accounts[0]
-        })
+      const instance = nonTradableInstance
       await instance.approve(accounts[0], 10, { from: accounts[0] });
       const tx = await instance.transferFrom(accounts[0], accounts[1], 10, { from: accounts[0] })
       assert.equal(tx.logs[0].event, "Transfer")
@@ -105,13 +101,7 @@ contract("ICOableToken", (accounts) => {
         new bn(1000000000 - 10).toString())
     })
     it("should allow transfer from seller during trade lock, when allowance is given to someone else", async () => {
-      let tradableAfter = new Date();
-      tradableAfter.setDate(tradableAfter.getDate() + 1);
-      const tradableAfterUnix = Math.round(tradableAfter.getTime() / 1000);
-      const instance = await ICOableToken.new(1000000000,
-        "bullshit token", 3, "BST", tradableAfterUnix, accounts[0], {
-          from: accounts[0]
-        })
+      const instance = nonTradableInstance
       // seller gives accounts[1] permission to spend 10 coins
       // accounts[1] then sends the 10 coins to accounts[2]
       await instance.approve(accounts[1], 10, { from: accounts[0] })
@@ -126,13 +116,7 @@ contract("ICOableToken", (accounts) => {
         new bn(1000000000 - 10).toString())
     })
     it("should not allow transfer from non-sellers", async () => {
-      let tradableAfter = new Date();
-      tradableAfter.setDate(tradableAfter.getDate() + 1);
-      const tradableAfterUnix = Math.round(tradableAfter.getTime() / 1000);
-      const instance = await ICOableToken.new(1000000000,
-        "bullshit token", 3, "BST", tradableAfterUnix, accounts[0], {
-          from: accounts[0]
-        })
+      const instance = nonTradableInstance
       // seller first gives accounts[1] 10 tokens
       await instance.transfer(accounts[1], 10, { from: accounts[0] })
       // accounts[1] gives allowance to accounts[2]
