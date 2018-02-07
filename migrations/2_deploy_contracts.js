9547b91055febf25cb7ad6ddd51136c46421b3d7
libertylocked <unlock.liberty@gmail.com>
2018-02-06 22:40:53 -0500

    Fix migration script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 const OneClickICO = artifacts.require("./OneClickICO.sol")
 
-module.exports = (deployer) => {
-  deployer.deploy(OneClickICO)
+module.exports = (deployer, network, accounts) => {
+  deployer.deploy(OneClickICO, accounts[0])
 };
