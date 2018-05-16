aa83e17e5ba280e300b158bbaf8ca3fa526c27e0
libertylocked <unlock.liberty@gmail.com>
2018-05-16 02:52:58 -0400

    add artifact to testnet

--- truffle-config.js
+++ truffle-config.js
@@ -1,10 +1,26 @@
+// const HDWalletProvider = require('truffle-hdwallet-provider')
+
 module.exports = {
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 100
+    }
+  },
   networks: {
     development: {
       host: 'localhost',
       port: 7545,
       gas: 6000000,
       network_id: 5777
+    },
+    ropsten: {
+      // provider: function() {
+      //   return new HDWalletProvider(seedPhrase, 'https://ropsten.infura.io/metamask', 1)
+      // },
+      gas: 4600000,
+      gasPrice: 1000000000,
+      network_id: 3
     }
   }
 };
